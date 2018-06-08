7583b1cd92cc445609e28e28f76bd4c31804c57d
yuva4297 <yuvaammu3@gmail.com>
2018-06-08 23:19:19 +0530

    Scorer class is added

--- src/Scorer.ts
+++ src/Scorer.ts
@@ -1,4 +1,26 @@
+import { Team } from "./Team";
+import { Batsman } from "./BatsMan";
+import { Bowler } from "./Bowler";
+
 export class Scorer
 {
+    listOfTeams: Array<Team>;
+    currentBowler: Bowler | null;
+    currentBattingTeam: Team | null;
+    currentBowlingTeam: Team | null;
+    currentNonStricker: Batsman | null;
+    currentStricker: Batsman | null;
+
+    constructor()
+    {
+       
+        this.listOfTeams = [];
+        this.currentBowler = null;
+        this.currentBattingTeam = null;
+        this.currentBowlingTeam = null;
+        this.currentStricker = null;
+        this.currentNonStricker = null;
+    }
+    
     
 }
